a79d3cf43d824be334995cbe1fe51cca26664f58
田中賢治 <key.m4096.kung.fu@gmail.com>
2015-12-18 15:53:48 +0900

    Define halloweenBGColor

--- SeasonalRefresh/UIColor+SeasonalRefresh.swift
+++ SeasonalRefresh/UIColor+SeasonalRefresh.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2015年 田中賢治. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    class func halloweenBGColor() -> UIColor {
+        return UIColor(red: 0.180, green: 0.247, blue: 0.5552, alpha: 1.0)
+    }
+}
